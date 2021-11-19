20bd2b07923aaf123050a5ccccbf0aadc66b393f
Ruhan Ahmad <93120026+Ruhan-Ahmad@users.noreply.github.com>
2021-11-19 15:52:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def route():
     return {
-        'name': [{"first-Name": "Ruhan", "last-Name": "Ahmad", "father-First-Name": "Faiz", "father-Last-Name": "Rasool"}]
+        'name': ["This app is for test purpose"}]
     }
 
 if __name__ == '__main__':
