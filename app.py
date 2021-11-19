407c0b13b5e4eb3b74a35e6a78a55cfbfebadcc3
Ruhan Ahmad <93120026+Ruhan-Ahmad@users.noreply.github.com>
2021-11-19 15:54:36 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def route():
     return {
-        'name': ["This app is for test purpose"}]
+        'name': ["This app is for test purpose"]
     }
 
 if __name__ == '__main__':
